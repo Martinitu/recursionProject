4545f8bb41a268306c911c9625b83babaf5461ec
Martinitu <martinitu@gmail.com>
2023-03-06 18:07:30 -0500

    iteration fibbonacci done

--- fibs.js
+++ fibs.js
@@ -1,1 +1,15 @@
 
+let fibs = function(num){
+    result = [];
+if (num == 1){
+    result.push(0)
+} else {
+    result.push(0)
+    result.push(1)
+            for (let i = 0; i < num - 2; i++)
+            result.push(result[i] + result[i + 1])
+            console.log(result)
+}
+}
+fibs(6);
+
