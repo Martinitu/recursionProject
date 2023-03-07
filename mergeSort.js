3d8b9a7db5e453b29b806b17d1937070417110cb
Martinitu <martinitu@gmail.com>
2023-03-07 18:59:21 -0500

    merge sort function addded

---
+++ mergeSort.js
@@ -0,0 +1,46 @@
+
+
+
+
+const mergeSort = (arr) => {
+    if (arr.length === 0) return "Please provide a non empty array!"
+	if (arr.length === 1) return arr;
+
+let m =Math.floor(arr.length / 2);
+ let    l =  arr.slice(0, m);
+ let r = arr.slice(m, arr.length);
+
+return (merge(mergeSort(l), mergeSort(r)))
+}
+
+let  merge = (lArr, rArr) => {
+let result = [];
+let i = 0;
+let j = 0; 
+while(i < lArr.length && j < rArr.length){
+    if (lArr[i] < rArr[j]){
+        result.push(lArr[i])
+        i++
+      
+    } else {
+            result.push(rArr[j])
+            j++
+           
+    }
+}
+    
+while (i < lArr.length){
+    result.push(lArr[i])
+    i++
+    
+}
+while (j < rArr.length){
+    result.push(rArr[j])
+    j++
+  
+}
+return result
+
+}
+
+console.log(mergeSort([10, 7, 8, 9, 1, 5, 3]))
