58e23ba0196332d51525f25abd6702a8a3610bfc
Martinitu <martinitu@gmail.com>
2023-03-06 19:08:07 -0500

    recursive method donde

--- fibs.js
+++ fibs.js
@@ -1,6 +1,6 @@
 
 let fibs = function(num){
-    result = [];
+   let result = [];
 if (num == 1){
     result.push(0)
 } else {
@@ -11,5 +11,17 @@ if (num == 1){
             console.log(result)
 }
 }
-fibs(6);
+fibs(8);
 
+function fibsRec(num) {
+    if (num <= 0 || isNaN(num)) {
+      return "Please enter a valid number."
+    } else if (num === 1) {
+        return [0]
+    } else if (num === 2) {
+        return [0, 1]
+    } else if (num > 2) {
+        return [...fibsRec(num-1), fibsRec(num - 1)[num - 2] + fibsRec(num-1)[num - 3]]
+    }
+  }
+console.log(fibsRec(8));
